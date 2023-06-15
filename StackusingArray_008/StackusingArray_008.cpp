2ec2c6deba901cb240f388453315eccdb5c43ff8
DzarZaki <dzarzaki2@gmail.com>
2023-06-15 18:09:23 +0700

    "Menambahkan logika pada fungsi display untuk menampilkan elemen-elemen dalam stack_array secara terurut dari atas ke bawah. Jika stack kosong, akan menampilkan pesan 'Stack is empty'

--- StackusingArray_008/StackusingArray_008.cpp
+++ StackusingArray_008/StackusingArray_008.cpp
@@ -46,3 +46,10 @@ public:
 		if (empty()) {
 			cout << "\nStack is empty." << endl;
 		}
+		else {
+			for (int tmp = top; tmp >= 0; tmp--) {
+				cout << stack_array[top] << endl;
+			}
+		}
+	}
+};
